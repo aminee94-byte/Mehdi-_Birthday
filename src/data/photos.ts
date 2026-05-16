6bfc81ee8ac1945428a05b55d79e002379ffed6d
aminee94-byte <148686590+aminee94-byte@users.noreply.github.com>
2026-05-17 01:20:02 +0200

    Add birthday photos data placeholder

---
+++ src/data/photos.ts
@@ -0,0 +1,8 @@
+export type BirthdayPhoto = {
+  title: string;
+  caption: string;
+  src: string;
+};
+
+export const birthdayPhotos: BirthdayPhoto[] = [];
+export const elbePhoto = birthdayPhotos[0];
